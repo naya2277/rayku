a1bedce85598f5270a44611092f856570cd906d6
Naya <nayaspam27@gmail.com>
2026-05-26 16:39:57 +0200

    Add PWA support

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener(
+  'install',
+  () => {
+    console.log('SW installed')
+  }
+)
+
+self.addEventListener(
+  'fetch',
+  () => {}
+)
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(
+  document.getElementById('root')!
+).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener(
+    'load',
+    () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .catch((err) => {
+          console.error(
+            'SW error',
+            err
+          )
+        })
+    }
+  )
+}
